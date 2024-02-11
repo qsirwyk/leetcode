b2d726139e68f4829198628d999f66dbb7b61357
qsir <qsir@mbp>
2024-02-11 21:29:46 +0800

    两数之和

--- editor/cn/1.two-sum.php
+++ editor/cn/1.two-sum.php
@@ -7,9 +7,18 @@ class Solution {
      * @param Integer $target
      * @return Integer[]
      */
-    function twoSum($nums, $target) {
-
+    function twoSum($nums, $target)
+    {
+        $hash = [];
+        foreach ($nums as $i => $iValue) {
+            if (isset($hash[$target - $iValue])) {
+                return [$hash[$target - $iValue], $i];
+            }
+            $hash[$iValue] = $i;
+        }
+        return [];
     }
+
 }
 //leetcode submit region end(Prohibit modification and deletion)
 
---
+++ editor/cn/350.intersection-of-two-arrays-ii.php
@@ -0,0 +1,15 @@
+<?php
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+
+    /**
+     * @param Integer[] $nums1
+     * @param Integer[] $nums2
+     * @return Integer[]
+     */
+    function intersect($nums1, $nums2) {
+
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
